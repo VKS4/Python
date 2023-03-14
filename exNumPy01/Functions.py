61abc8a0d5158d88465dd0bc2f08718edeaf5c47
VKS4 <workvaksa@gmail.com>
2023-03-14 11:04:45 +0100

    updating the file with new exercises

--- exNumPy01/Functions.py
+++ exNumPy01/Functions.py
@@ -1,10 +1,45 @@
+# import numpy library
 import numpy
 
 
+# define a function load_array_x
 def load_array_x():
+    # csv file with name DataPythonProject.csv
     file = open("DataPythonProject.csv")
-    array_x = numpy.genfromtxt(file, dtype="i8", delimiter=",")
-    print("printing array_x\n", array_x)
 
+    # using function genfromtxt from the numpy library and change the data type of the array from original float to int8
+    array_x = numpy.genfromtxt(file, dtype="i8", delimiter=",")
+
+    # print array_x after initial message
+    print("printing array_x \n {}" .format(array_x))
+
+    # use abs() function to convert any negative numbers to positive values
     array_y = abs(array_x)
-    print("printing array_y\n", array_y)
+
+    # print the resulting array_y
+    print("printing array_y \n {}" .format(array_y))
+
+    # access the first row and first column of array_y and store in a variable element
+    element = array_y[0][0]
+
+    # print the variable element after initial message
+    print("printing 1st row and a first column of array_y \n {}" .format(element))
+
+    # access several cells of an array_y
+    element_range_a = array_y[3][:]
+
+    # printing variable element_range_a
+    print("printing 4th row and all columns of array_y \n {}" .format(element_range_a))
+
+    # access first 4 columns of row 5 in array_y
+    element_range_b = array_y[4][:4]
+
+    # print variable element_range_b
+    print("printing first four columns in the 5th row of array_y \n {}" .format(element_range_b))
+
+    # # access all data between 4th row and 4th column of array_y
+    # element_range_c = array_y[:4][:]
+    # element_range_c = array_y[:][:4]
+    #
+    # # print the variable element_range_c
+    # print("printing section of array_y \n {}" .format(element_range_c))
--- exNumPy01/main.py
+++ exNumPy01/main.py
@@ -1,12 +1,12 @@
 import sys
 import numpy
-import pandas
 import matplotlib
 import Functions
 
-print('Python: {}'.format(sys.version))
-print('Numpy: {}' .format(numpy.__version__))
-print('Pandas: {}' .format(pandas.__version__))
-print('Plotlib: {}' .format(matplotlib.__version__))
+# print versions of imported libraries using string formatting
+print("Python: {}".format(sys.version))
+print("Numpy: {}" .format(numpy.__version__))
+print("Plotlib: {}" .format(matplotlib.__version__))
 
+# calling function load_array_x from python file Functions
 Functions.load_array_x()
