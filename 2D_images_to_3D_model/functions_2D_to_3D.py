5c0a85f9d804f220c4d9be00721463169061ddad
VKS4 <workvaksa@gmail.com>
2023-03-30 14:02:02 +0200

    update with image filtering to find edges using PIL library

--- 2D_images_to_3D_model/functions_2D_to_3D.py
+++ 2D_images_to_3D_model/functions_2D_to_3D.py
@@ -1,21 +1,26 @@
 import os
-from PIL import Image
+from PIL import Image, ImageFilter
+import matplotlib.pyplot as plt
+import numpy
 
 
 class ImageToGreyscale:
     def __init__(self):
+
         # loading up a .jpeg image by calling on a system variable, needs to be set up through OS. The system variable
         # contains path to the image. E.g. C:\Documents\...
         # self. makes a variable an instance variable that can be accessed by other methods in the class
         self.image_path = os.environ.get("IMAGE_PATH")
 
-    @staticmethod
-    def load_image(image_path):
+    @classmethod
+    def load_image(cls, image_path):
+
         # using PIL library to open the image
         image = Image.open(image_path)
 
         try:
-            # line bellow is used to check if the system variable leads to the image file
+            # line bellow is used to check if the system variable leads to the image file. If the image path is wrong
+            # the program will return exception
             image.show()
         except Exception as e:
             print(f"Error: {e}")
@@ -24,8 +29,9 @@ class ImageToGreyscale:
 
     @staticmethod
     def convert_image_to_greyscale(image):
+
         # convert uploaded image to a bit map with greyscale values
-        bitmap = image.convert("1")
+        bitmap = image.convert("L")
 
         # loading up the image converted to a bitmap from main file
         pixels = bitmap.load()
@@ -33,4 +39,38 @@ class ImageToGreyscale:
         # obtaining the width and the height of the bitmap
         width, height = bitmap.size
 
-        return pixels, width, height
+        # print the width and height of the picture
+        print(f"width {width}\nheight {height}")
+
+        return pixels, width, height, bitmap
+
+    @staticmethod
+    def create_greyscale_plot_using_for_loop(pixels, width, height):
+
+        # using for loop to go through the PixelAccess object
+        gray_pixels = [[pixels[x, y] for x in range(width)] for y in range(height)]
+        gray_pixels = numpy.array(gray_pixels, dtype=numpy.uint8)
+        processed_image = Image.fromarray(gray_pixels)
+
+        plt.suptitle("Greyscale of the uploaded image")
+        plt.imshow(gray_pixels, cmap='gray')
+        plot_using_for = plt.show()
+
+        processed_image = processed_image.filter(ImageFilter.CONTOUR)
+        processed_image.show()
+
+        return plot_using_for, processed_image
+
+    # @staticmethod
+    # def create_greyscale_plot_using_numpy(bitmap):
+    #
+    #     # convert the PixelAccess object to a numpy array of type uint8
+    #     pixels = numpy.array(bitmap)
+    #
+    #     # convert the PixelAccess object to a numpy array
+    #     gray_pixels = numpy.array(pixels)
+    #
+    #     plt.imshow(gray_pixels, cmap='gray')
+    #     plot_using_numpy_array = plt.show()
+    #
+    #     return plot_using_numpy_array
--- 2D_images_to_3D_model/main.py
+++ 2D_images_to_3D_model/main.py
@@ -1,5 +1,25 @@
 import functions_2D_to_3D
 
 
-image_to_greyscale = functions_2D_to_3D.ImageToGreyscale()
-image_to_greyscale.load_image(image_to_greyscale.image_path)
+def convert_image_to_greyscale():
+    # create an instance of the class ImageToGreyscale
+    img_to_greyscale = functions_2D_to_3D.ImageToGreyscale()
+
+    # call the load_image() method
+    image = img_to_greyscale.load_image(img_to_greyscale.image_path)
+
+    # call the convert_image_to_greyscale() method
+    pixels, width, height, bitmap = functions_2D_to_3D.ImageToGreyscale.convert_image_to_greyscale(image)
+
+    # call method from class ImageToGreyscale that uses for loop to access the data in PixelAccess object
+    plot_using_for, processed_image = functions_2D_to_3D.ImageToGreyscale.create_greyscale_plot_using_for_loop(pixels,
+                                                                                                               width,
+                                                                                                               height)
+
+    # # call method from class ImageToGreyscale that uses numpy array to access the data in PixelAccess object
+    # plot_using_numpy_array = functions_2D_to_3D.ImageToGreyscale.create_greyscale_plot_using_numpy(pixels)
+
+    return plot_using_for, processed_image
+
+
+convert_image_to_greyscale()
