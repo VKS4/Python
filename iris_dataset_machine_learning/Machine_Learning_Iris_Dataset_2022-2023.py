40a68c06b9b3932b1e01be22a3e197d3939086bc
VKS4 <workvaksa@gmail.com>
2023-04-05 17:13:53 +0200

    Update with latest version of the machine learning project

--- iris_dataset_machine_learning/Machine_Learning_Iris_Dataset_2022-2023.py
+++ iris_dataset_machine_learning/Machine_Learning_Iris_Dataset_2022-2023.py
@@ -10,7 +10,8 @@ from matplotlib import pyplot
 from pandas import DataFrame
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -54,40 +55,26 @@ def plot_dataset(iris_dataset):
     pandas.plotting.scatter_matrix(iris_dataset)
     pyplot.show()
 
-    array = iris_dataset.values
-    x = array[:, 0:4]
-    y = array[:, 4]
+    dataset = iris_dataset.values
+    x = dataset[:, 0:4]
+    y = dataset[:, 4]
 
-    return x, y, array
+    return x, y, dataset
 
 
-def split_test(x, y, array):
+def split_test(x, y, dataset):
     print('Length of array X', len(x))
     print('Length of array Y', len(y))
 
-    # split the dataset into 20% and 80%. (learning dataset part and testing dataset part)
-    a = len(x) * 0.2
-    b = len(y) * 0.2
+    dataset_array = pandas.DataFrame(dataset).values
+    X = dataset_array[:, 0:4]
+    y = dataset_array[:, 4]
+    train_x, validation_x, train_y, validation_y = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
 
-    a2 = len(x) * 0.8
-    b2 = len(y) * 0.8
-
-    learn_x = array[0:int(a2), 0:4]
-    learn_y = array[0:int(b2), 4]
-
-    test_x = array[0:int(a), 0:4]
-    test_y = array[0:int(b), 4]
-
-    print('Test X array ', test_x)
-    print('Test Y array ', test_y)
-
-    print('Learn X array ', learn_x)
-    print('Learn Y array ', learn_y)
-
-    return learn_x, learn_y, test_x, test_y
+    return train_x, validation_x, train_y, validation_y
 
 
-def test_models(learn_x, learn_y):
+def test_models(train_x, train_y):
     models = [('LR', LogisticRegression(solver='liblinear', multi_class='ovr')),
               ('LDA', LinearDiscriminantAnalysis()),
               ('KNN', KNeighborsClassifier()),
@@ -101,7 +88,7 @@ def test_models(learn_x, learn_y):
 
     for name, model in models:
         kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-        cv_results = cross_val_score(model, learn_x, learn_y, cv=kfold, scoring='accuracy')
+        cv_results = cross_val_score(model, train_x, train_y, cv=kfold, scoring='accuracy')
         results.append(cv_results)
         names.append(name)
 
@@ -110,7 +97,48 @@ def test_models(learn_x, learn_y):
     return models, results, names, cv_results
 
 
-names_out, iris_dataset_out = load_dataset()
-X, Y, array = plot_dataset(iris_dataset_out)
-learn_x, learn_y, test_x, test_y = split_test(X, Y, array)
-models, results, names, cv_results = test_models(learn_x, learn_y)
+def make_predictions(models, results, names, cv_results):
+    names_out, iris_dataset_out = load_dataset()
+    X, Y, array = plot_dataset(iris_dataset_out)
+    train_x, validation_x, train_y, validation_y = split_test(X, Y, array)
+    models, results, names, cv_results = test_models(train_x, train_y)
+
+    # Make predictions on validation dataset
+    model = SVC(gamma='auto')
+    model.fit(train_x, train_y)
+    predictions = model.predict(validation_x)
+
+    # Evaluate predictions
+    print(accuracy_score(validation_y, predictions))
+    print(confusion_matrix(validation_y, predictions))
+    print(classification_report(validation_y, predictions))
+
+    pass
+
+
+def main():
+    # Load the dataset
+    names, iris_dataset = load_dataset()
+
+    # Plot the dataset
+    X, Y, array = plot_dataset(iris_dataset)
+
+    # Split the dataset into training and validation sets
+    train_x, validation_x, train_y, validation_y = split_test(X, Y, array)
+
+    # Test different models on the training set
+    models, results, names, cv_results = test_models(train_x, train_y)
+
+    # Make predictions on the validation set using the best model
+    model = SVC(gamma='auto')
+    model.fit(train_x, train_y)
+    predictions = model.predict(validation_x)
+
+    # Evaluate the predictions
+    print(accuracy_score(validation_y, predictions))
+    print(confusion_matrix(validation_y, predictions))
+    print(classification_report(validation_y, predictions))
+
+
+if __name__ == '__main__':
+    main()
