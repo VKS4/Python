7010ec804a48215438e572f17bf24661447c3f47
VKS4 <workvaksa@gmail.com>
2023-04-06 19:07:25 +0200

    update with improved code structure and added comments for the machine learning project

--- iris_dataset_machine_learning/Machine_Learning_Iris_Dataset_2022-2023.py
+++ iris_dataset_machine_learning/Machine_Learning_Iris_Dataset_2022-2023.py
@@ -1,4 +1,4 @@
-# check package versions
+# import libraries
 import sys
 
 import matplotlib
@@ -18,19 +18,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
-print('Python: {}'.format(sys.version))
-print('scipy: {}'.format(scipy.__version__))
-print('numpy: {}'.format(numpy.__version__))
-print('matplotlib: {}'.format(matplotlib.__version__))
-print('pandas: {}'.format(pandas.__version__))
-print('sklearn: {}'.format(sklearn.__version__))
+# check package versions
+print("Python: {}".format(sys.version))
+print("scipy: {}".format(scipy.__version__))
+print("numpy: {}".format(numpy.__version__))
+print("matplotlib: {}".format(matplotlib.__version__))
+print("pandas: {}".format(pandas.__version__))
+print("sklearn: {}".format(sklearn.__version__))
 
 
+# declaration of a function that loads up the dataset
 def load_dataset():
-    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    # create a list of names of the features of the iris flowers
+    names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
+
+    # load the dataset from local file iris.csv using the pandas library
     iris_dataset: DataFrame | None = pandas.read_csv("iris.csv")
 
-    # print how many instances(rows) and attributes(columns) does the data set have
+    # print how many rows and columns does the dataset have
     print(iris_dataset.shape)
 
     # print the first 20 lines of data of all columns
@@ -39,42 +44,67 @@ def load_dataset():
     # print statistic information about the dataset
     print(iris_dataset.describe())
 
+    # return the variables from the function
     return names, iris_dataset
 
 
+# declaration of a function that plots the data from the dataset
 def plot_dataset(iris_dataset):
-    # box and whisker plots
+    # create box and whisker plots
     iris_dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
     pyplot.show()
 
-    # histograms
+    # create histograms
     iris_dataset.hist()
     pyplot.show()
 
-    # scatter plot matrix
+    # create scatter plot matrix
     pandas.plotting.scatter_matrix(iris_dataset)
     pyplot.show()
 
+    # use pandas libray to extract values from the iris_dataset and load them up to dataset variable as a numpy array
     dataset = iris_dataset.values
+
+    # assign values from the dataset array to the variable x (all rows, and first 4 columns (0 to 3))
     x = dataset[:, 0:4]
+
+    # assign values from dataset to the variable y (all rows, 3rd column)
     y = dataset[:, 4]
 
+    # return the variables from the function
     return x, y, dataset
 
 
-def split_test(x, y, dataset):
+# declaration of a function that splits the dataset into training and validation part
+def split_dataset(x, y, dataset):
+    # print the length of array x
     print('Length of array X', len(x))
+
+    # print the length of array y
     print('Length of array Y', len(y))
 
+    # use pandas library to create a new numpy array from the dataset array with included metadata
     dataset_array = pandas.DataFrame(dataset).values
-    X = dataset_array[:, 0:4]
-    y = dataset_array[:, 4]
-    train_x, validation_x, train_y, validation_y = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
 
+    # assign values to the variable x from the array dataset_array (all rows, first 4 columns(first index is inclusive,
+    # stop index is exclusive))
+    x = dataset_array[:, 0:4]
+
+    # assign values to the variable y from the array dataset_array (all rows, 5th column(4th index))
+    # contains the names of the flowers (used to check accuracy of the model)
+    y = dataset_array[:, 4]
+
+    # split the x and y arrays into training (80%) and validation (20%) sets, set that the same sequence of data is used
+    # always, shuffles the dataset before splitting
+    train_x, validation_x, train_y, validation_y = train_test_split(x, y, test_size=0.20, random_state=1, shuffle=True)
+
+    # return the variables from the function
     return train_x, validation_x, train_y, validation_y
 
 
+# declaration of a function that will test the accuracy of models
 def test_models(train_x, train_y):
+    # loads the machine learning models into memory as a list of tuples
     models = [('LR', LogisticRegression(solver='liblinear', multi_class='ovr')),
               ('LDA', LinearDiscriminantAnalysis()),
               ('KNN', KNeighborsClassifier()),
@@ -82,26 +112,44 @@ def test_models(train_x, train_y):
               ('NB', GaussianNB()),
               ('SVM', SVC(gamma='auto'))]
 
+    # declares empty lists results, names and cv_results
     results = []
     names = []
     cv_results = []
 
+    # unpacking of the list of tuples, name will be assigned first value from the tuple (string with the name of the
+    # model) and model will be assigned the second value in the tuple (the model itself)
     for name, model in models:
+        # use kfold cross-validation to evaluate the performance of the models
         kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+
+        # use cross validation function from the scikit-learn library to evaluate the models
+        # new model for each iteration
         cv_results = cross_val_score(model, train_x, train_y, cv=kfold, scoring='accuracy')
+
+        # append cv_results to the empty list results
         results.append(cv_results)
+
+        # append name to the empty list names
         names.append(name)
 
+        # print the name of the model, mean value of cv_results and average value of cv_results
         print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
+    # return the variables of the function
     return models, results, names, cv_results
 
 
-def make_predictions(models, results, names, cv_results):
+# declaration of a function for making predictions based on the trained data
+def make_predictions():
+    # creating an instance of the function load_order
     names_out, iris_dataset_out = load_dataset()
+
+    # creating an instance of the function plot_dataset
     X, Y, array = plot_dataset(iris_dataset_out)
-    train_x, validation_x, train_y, validation_y = split_test(X, Y, array)
-    models, results, names, cv_results = test_models(train_x, train_y)
+
+    # creating an instance of the function split_dataset
+    train_x, validation_x, train_y, validation_y = split_dataset(X, Y, array)
 
     # Make predictions on validation dataset
     model = SVC(gamma='auto')
@@ -117,14 +165,14 @@ def make_predictions(models, results, names, cv_results):
 
 
 def main():
-    # Load the dataset
+    # creating an instance of the function load_dataset
     names, iris_dataset = load_dataset()
 
-    # Plot the dataset
+    # creating an instance of the function plot_dataset
     X, Y, array = plot_dataset(iris_dataset)
 
-    # Split the dataset into training and validation sets
-    train_x, validation_x, train_y, validation_y = split_test(X, Y, array)
+    # creating an instance of the function split_dataset
+    train_x, validation_x, train_y, validation_y = split_dataset(X, Y, array)
 
     # Test different models on the training set
     models, results, names, cv_results = test_models(train_x, train_y)
@@ -139,6 +187,9 @@ def main():
     print(confusion_matrix(validation_y, predictions))
     print(classification_report(validation_y, predictions))
 
+    pass
 
+
+# condition, if the name of the file is main, then execute function main
 if __name__ == '__main__':
     main()
